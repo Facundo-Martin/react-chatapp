3c3aaaa5b8299a63b2b588088716d19324fecbbc
Facundo Martin <facundomartin98@gmail.com>
2022-04-25 11:57:51 -0400

    added sidebar header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Imessage from "./Imessage";
 
 function App() {
   return (
     <div className="app">
-      <h1>React Chat App</h1>
+      <Imessage />
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,26 @@
+import { Avatar, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import RateReviewIcon from "@mui/icons-material/RateReview";
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar className="sidebar__avatar" />
+        <div className="sidebar__input">
+          <SearchIcon />
+          <input placeholder="Search" />
+        </div>
+
+        <IconButton variant="outlined" className="sidebar__inputButton">
+          <RateReviewIcon />
+        </IconButton>
+      </div>
+      <div className="sidebar__chats"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
