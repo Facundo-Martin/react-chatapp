6979666ee2e282052c87a3ea80eda81c67cd21c2
Facundo Martin <facundomartin98@gmail.com>
2022-05-04 09:56:07 -0400

    added GoogleAuth to Login component

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,44 @@
+import { Button } from "@mui/material";
 import React from "react";
 import "./Login.css";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth, provider } from "./firebase";
 
 function Login() {
+  const googleHandler = async () => {
+    provider.setCustomParameters({ prompt: "select_account" });
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // redux action? --> dispatch({ type: SET_USER, user });
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
   return (
     <div className="login">
       <div className="login__logo">
-        <img src="" alt="" />
+        <img
+          src="https://i.postimg.cc/Xv9J138r/IMessage-logo-Apple-Inc.png"
+          alt="Chatsio logo"
+        />
         <h1>Chatsio</h1>
       </div>
+
+      <Button onClick={googleHandler}>Sign In</Button>
     </div>
   );
 }
