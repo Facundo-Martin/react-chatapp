c12dacc8e527b5de96f6681e8801d0b061105be2
facundomartin <facundomartin98@gmail.com>
2022-05-09 20:20:27 -0400

    added deleteChat from Firestore

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,18 +1,28 @@
-import { Avatar } from "@mui/material";
+import { Avatar, Button } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import "./SidebarChat.css";
 import { useDispatch } from "react-redux";
 import { setChat } from "./features/chatSlice";
 // Firebase imports
 import db from "./firebase";
-import { collection, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  deleteDoc,
+  doc,
+  updateDoc,
+  deleteField,
+} from "firebase/firestore";
 import { query, orderBy } from "firebase/firestore";
 import * as timeago from "timeago.js";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function SidebarChat({ id, chatName }) {
   const dispatch = useDispatch();
   const [chatInfo, setChatInfo] = useState([]);
-  // Firestore Ref
+  // Firestore Refs
+  const colRef = collection(db, "chats");
+
   const messageRef = collection(db, "chats", `${id}`, "messages");
 
   useEffect(() => {
@@ -21,6 +31,14 @@ function SidebarChat({ id, chatName }) {
       setChatInfo(snapshot.docs.map((doc) => doc.data()));
     });
   }, [id]);
+
+  console.log("This is the chatName", { id });
+  const deleteChat = () => {
+    const docRef = doc(db, "chats", id);
+    if (id !== "rnyRbNBSfNXakMuVmyXW") {
+      deleteDoc(docRef);
+    }
+  };
 
   return (
     <div
@@ -38,12 +56,16 @@ function SidebarChat({ id, chatName }) {
       <div className="sidebarChat__info">
         <h3> {chatName}</h3>
         <p>{chatInfo[0]?.message}</p>
+
         <small>
           {chatInfo[0]
             ? timeago.format(new Date(chatInfo[0].timestamp?.toDate()))
-            : "No Message"}
+            : "No Messages"}
         </small>
       </div>
+      <Button className="sidebarChat__delete" onClick={deleteChat}>
+        <DeleteIcon style={{ color: "grey" }} />
+      </Button>
     </div>
   );
 }
